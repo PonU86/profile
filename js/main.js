756424bf44101a7d3feae144ead9fa98e1f916b6
Preston Hostetler <Hostetlerp@gmail.com>
2023-04-26 12:29:07 -0600

    commit
    commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,112 @@
+// DOM ELEMENTS
 
+const time = document.getElementById('time'),
+greeting = document.getElementById('greeting'),
+name = document.getElementById('name'),
+focus = document.getElementById('focus');
+
+//Options
+const showAmPm = true;
+
+
+// Show Time
+function showTime() {
+    let today = new Date(),
+    hour = today.getHours(),
+    min = today.getMinutes(),
+    sec = today.getSeconds();
+
+    // Set AM or PM
+    const amPm = hour >= 12 ? 'PM' : 'AM';
+
+    // 12hr Format
+    // hour = hour % 12 || 12;
+
+    //Output time
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+    // Add inside last ' if you want to show AM PM ${showAmPm ? amPm : ''}
+
+    setTimeout(showTime, 1000);
+}
+
+// Add Zero
+function addZero(n) {
+    return (parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+
+// Set Background and Greeting
+function setBgGreet() {
+    let today = new Date(),
+    hour = today.getHours();
+
+    if(hour < 12) {
+        // Morning
+        document.body.style.backgroundColor = "lightblue";
+        greeting.textContent = 'Good Morning';
+        document.body.style.color = 'white';
+    } else if(hour < 18) {
+        // Afternoon
+        document.body.style.backgroundColor = "blue";
+        greeting.textContent = 'Good Afternoon';
+    } else {
+        // Evening
+        document.body.style.backgroundColor = "black";
+        greeting.textContent = 'Good Evening';
+        document.body.style.color = 'white';
+    }
+}
+
+// Get Name
+function getName() {
+    if(localStorage.getItem('name') === null) {
+        name.textContent = '[Enter Name]';
+    } else {
+        name.textContent = localStorage.getItem('name');
+    }
+}
+
+// Set Name
+function setName(e) {
+    if(e.type === 'keypress') {
+        // Make sure enter is pressed
+        if(e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('name', e.target.innerText);
+            name.blur();
+        }
+    } else {
+        localStorage.setItem('name', e.target.innerText);
+    }
+}
+
+// Get Focus
+function getFocus() {
+    if(localStorage.getItem('focus') === null) {
+        focus.textContent = '[Insperation for the day!]';
+    } else {
+        focus.textContent = localStorage.getItem('focus');
+    }
+}
+
+// Set Focus
+function setFocus(e) {
+    if(e.type === 'keypress') {
+        // Make sure enter is pressed
+        if(e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('focus', e.target.innerText);
+            name.blur();
+        }
+    } else {
+        localStorage.setItem('focus', e.target.innerText);
+    }
+}
+
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+
+// Run
+showTime();
+setBgGreet();
+getName();
+getFocus();
